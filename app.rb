107a59cd48c00500d3f8cb7b4c37a21027d38ef7
Kate <katepdonahue@gmail.com>
2013-12-13 10:17:43 -0500

    It almost works

--- app.rb
+++ app.rb
@@ -11,11 +11,18 @@ class App < Sinatra::Application
   post '/result' do
     array = []
     hash = {}
-    params.each {|k, v| array << v}
-    array.each do |elt|
-      hash[elt] = array.count(elt)
+    params.each {|k, v| array << v; hash[v] = 0}
+    hash.each do |k, v|
+      hash[k] = array.count(k)
+      puts "You answered #{k} #{array.count(k)} times"
     end
-    @result = hash.max_by{|k,v| v}
+    puts hash.count
+    puts hash.keys
+    @result = hash.keys[0] if hash.count == 1
+    puts "result is #{@result}"
+    @result ||= hash.max_by{|k,v| v}[0]
+    # if hash.max_by{|k,v| v} first two keys are the same, puts rainbow ranger
+    puts @result
     erb :result
   end
  
